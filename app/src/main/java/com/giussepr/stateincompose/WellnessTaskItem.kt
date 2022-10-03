3a7b48df3b61cab140a646576f73c30cfc7e9156
Giussep Ricardo <giussep@global.id>
2022-10-03 15:20:26 -0500

    Create WellnessTaskList and WellnessTask data model, add checkbox to WellnessTaskItem and show WellnessTaskList on WellnessScreen

--- app/src/main/java/com/giussepr/stateincompose/WellnessTaskItem.kt
+++ app/src/main/java/com/giussepr/stateincompose/WellnessTaskItem.kt
@@ -2,31 +2,61 @@ package com.giussepr.stateincompose
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.giussepr.stateincompose.ui.theme.StateInComposeTheme
+
+@Composable
+@Preview
+fun WellnessTaskItemPreview() {
+    StateInComposeTheme {
+        WellnessTaskItem(taskName = "Task one", onClose = { }, onCheckedChange = {})
+    }
+}
 
 @Composable
 fun WellnessTaskItem(
+    modifier: Modifier = Modifier,
     taskName: String,
+    isChecked: Boolean = false,
     onClose: () -> Unit,
-    modifier: Modifier = Modifier
+    onCheckedChange: (Boolean) -> Unit
 ) {
     Row(
         modifier = modifier, verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            modifier = Modifier.weight(1f).padding(start = 16.dp),
+            modifier = Modifier
+                .weight(1f)
+                .padding(start = 16.dp),
             text = taskName
         )
+        Checkbox(checked = isChecked, onCheckedChange = onCheckedChange)
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
+}
+
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable { mutableStateOf(false) }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        isChecked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue },
+        onClose = {}, // we will implement this later!
+        modifier = modifier,
+    )
 }
---
+++ app/src/main/java/com/giussepr/stateincompose/model/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.giussepr.stateincompose.model
+
+data class WellnessTask(val id: Int, val label: String)
